c0c153e628e68683d36b6634b4b5161b0a71cc62
Nael <willowitzy@gmail.com>
2023-11-03 04:44:29 -0300

    finished chapter3

--- chapter3/constants_and_enums.cpp
+++ chapter3/constants_and_enums.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
-int main() {
-    
+// Does #define MAX = 100 work too?
+#define MAX 100
 
+int main() {
+    const int MAX_VALUE = 100;
+    
+    // Enumerators
+    enum Color { 
+        RED,    // = 0
+        GREEN,  // = 1
+        BLUE,   // = 2
+        PINK,   // = 3
+        ORANGE, // = 4
+        YELLOW, // = 5
+        WHITE,  // = 6
+        BLACK   // = 7
+    };
 
     return 0;
 }
